21c265edeab43e41b51c3fb517de492d2e50c331
Juan Francisco Gramajo <juanfranciscogramajounlp@gmail.com>
2026-04-05 19:51:04 -0300

    ejercicio 10 simulacion de competencia de cocina y ranking terminado

--- src/Simulación_de_competencia_de_cocina_y_ranking.py
+++ src/Simulación_de_competencia_de_cocina_y_ranking.py
@@ -75,3 +75,37 @@ rounds = [
 }
 }
 ]
+ranking = { 'Valentina':{'Puntaje': 0, 'Rondas ganadas': 0, 'Mejor ronda':0, 'Promedio': 0.0},
+           'Lucía':{'Puntaje': 0, 'Rondas ganadas': 0, 'Mejor ronda':0, 'Promedio': 0.0},
+           'Mateo':{'Puntaje': 0, 'Rondas ganadas': 0, 'Mejor ronda':0, 'Promedio': 0.0},
+           'Camila':{'Puntaje': 0, 'Rondas ganadas': 0, 'Mejor ronda':0, 'Promedio': 0.0},
+           'Santiago':{'Puntaje': 0, 'Rondas ganadas': 0, 'Mejor ronda':0, 'Promedio': 0.0}}
+i = 1
+for ronda in rounds:
+    puntaje_ronda = ronda['scores']
+    ganador_puntaje = 0
+    ganador_nombre = ''
+    for chef in puntaje_ronda:
+        puntaje_jueces = puntaje_ronda[chef]
+        puntaje_total = sum(puntaje_jueces.values())
+        if ganador_puntaje < puntaje_total:
+            ganador_nombre = chef
+            ganador_puntaje = puntaje_total
+        ranking[chef]['Puntaje'] += puntaje_total
+        if ranking[chef]['Mejor ronda'] < puntaje_total:
+            ranking[chef]['Mejor ronda'] = puntaje_total
+        ranking[chef]['Promedio'] = round(ranking[chef]['Puntaje'] / i, 2)
+    ranking[ganador_nombre]['Rondas ganadas'] += 1   
+    print(f"Ronda {i} - {ronda['theme']}:\n Ganador: {ganador_nombre} ({ganador_puntaje} pts)")
+    print(f" tabla de posiciones: \n")
+    for chef, stats in ranking.items():
+        print(f"{chef}: Puntaje: {stats['Puntaje']}, Rondas ganadas: {stats['Rondas ganadas']}, Mejor ronda: {stats['Mejor ronda']}, Promedio: {stats['Promedio']}")
+    i += 1
+
+print('Tabla final de posiciones final:')
+# Ordenamos el ranking: 'reverse=True' para que el mayor puntaje vaya primero
+ranking_final = sorted(ranking.items(), key=lambda x: x[1]['Puntaje'], reverse=True)
+
+for chef, stats in ranking_final:
+    print(f"{chef}: Puntaje: {stats['Puntaje']}, Rondas ganadas: {stats['Rondas ganadas']}, Mejor ronda: {stats['Mejor ronda']}, Promedio: {stats['Promedio']}")
+    
