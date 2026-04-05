8f90824c5913854a9477a7c69001f67c8123a9a3
Juan Francisco Gramajo <juanfranciscogramajounlp@gmail.com>
2026-04-05 16:35:05 -0300

    creacion de punto 10

---
+++ src/Simulación_de_competencia_de_cocina_y_ranking.py
@@ -0,0 +1,77 @@
+rounds = [
+{
+'theme': 'Entrada',
+'scores': {
+'Valentina': {'judge_1': 8, 'judge_2': 7,
+'judge_3': 9},
+'Mateo': {'judge_1': 7, 'judge_2': 8,
+'judge_3': 7},
+'Camila': {'judge_1': 9, 'judge_2': 9,
+'judge_3': 8},
+'Santiago': {'judge_1': 6, 'judge_2': 7,
+'judge_3': 6},
+'Lucía': {'judge_1': 8, 'judge_2': 8,
+'judge_3': 8},
+}
+},
+{
+'theme': 'Plato principal',
+'scores': {
+'Valentina': {'judge_1': 9, 'judge_2': 9,
+'judge_3': 8},
+'Mateo': {'judge_1': 8, 'judge_2': 7,
+'judge_3': 9},
+'Camila': {'judge_1': 7, 'judge_2': 6,
+'judge_3': 7},
+'Santiago': {'judge_1': 9, 'judge_2': 8,
+'judge_3': 8},
+'Lucía': {'judge_1': 7, 'judge_2': 8,
+'judge_3': 7},
+}
+},
+{
+'theme': 'Postre',
+'scores': {
+'Valentina': {'judge_1': 7, 'judge_2': 8,
+'judge_3': 7},
+'Mateo': {'judge_1': 9, 'judge_2': 9,
+'judge_3': 8},
+'Camila': {'judge_1': 8, 'judge_2': 7,
+'judge_3': 9},
+'Santiago': {'judge_1': 7, 'judge_2': 7,
+'judge_3': 6},
+'Lucía': {'judge_1': 9, 'judge_2': 9,
+'judge_3': 9},
+}
+},
+{
+'theme': 'Cocina internacional',
+'scores': {
+'Valentina': {'judge_1': 8, 'judge_2': 9,
+'judge_3': 9},
+'Mateo': {'judge_1': 7, 'judge_2': 6,
+'judge_3': 7},
+'Camila': {'judge_1': 9, 'judge_2': 8,
+'judge_3': 8},
+'Santiago': {'judge_1': 8, 'judge_2': 9,
+'judge_3': 7},
+'Lucía': {'judge_1': 7, 'judge_2': 7,
+'judge_3': 8},
+}
+},
+{
+'theme': 'Final libre',
+'scores': {
+'Valentina': {'judge_1': 9, 'judge_2': 8,
+'judge_3': 9},
+'Mateo': {'judge_1': 8, 'judge_2': 9,
+'judge_3': 8},
+'Camila': {'judge_1': 7, 'judge_2': 7,
+'judge_3': 7},
+'Santiago': {'judge_1': 9, 'judge_2': 9,
+'judge_3': 9},
+'Lucía': {'judge_1': 8, 'judge_2': 8,
+'judge_3': 7},
+}
+}
+]
